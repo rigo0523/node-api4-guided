952a45718e352e7ae9da8d179f002c1fd529b815
Sean Kirkby <sean.kirkby@niceincontact.com>
2020-07-16 09:05:07 -0600

    adjustment

--- api/api-router.js
+++ api/api-router.js
@@ -20,13 +20,14 @@ router.get("/", async (req, res, next) => {
     // variables that Heroku will create in the OS environment it makes for your
     // app by clicking on the "Settings" tab for your app in Heroku.
     //
-    // Our app will not be retrieving the PORT variable from a definition in this
-    // way... Heroku automatically creates the PORT environment variable for us
-    // (we don't need to tell it to). But there are times when we still want other
-    // configuration environment variables that our app can use. And since we
-    // don't have access to the shell environment before our application is
-    // executed by Heroku, we need to define the variables we want Heroku to
-    // create and export for our app using this feature (Settings). 
+    // Our app will not be retrieving the PORT variable from a definition in
+    // this way... Heroku automatically creates the PORT environment variable
+    // for us (we don't need to tell it to). But there are times when we still
+    // want other configuration environment variables that our app can use. And
+    // since we don't have access to the shell environment before our
+    // application is executed by Heroku, we need to define the variables we
+    // want Heroku to create and export for our app using this feature
+    // (Settings). 
     const messageOfTheDay = process.env.MOTD || 'Helo World!';
     const shouts = await Shouts.find();
     res.status(200).json({ message: messageOfTheDay, shouts });
