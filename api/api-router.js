ba1e2fbb7ed46be7fe1b6aa228a6d2a1af76ecb7
Sean Kirkby <skrkby@kirkby.org>
2020-11-04 20:57:16 -0700

    added motd feature

--- api/api-router.js
+++ api/api-router.js
@@ -8,7 +8,9 @@ router.use(express.json());
 
 // comments for fun
 router.get("/", (req, res) => {
-  res.status(200).json({ api: "up" });
+  const motd = process.env.MOTD || 'hello world!';
+
+  res.status(200).json({ api: "up", motd: motd });
 });
 
 router.get("/shouts", (req, res, next) => {
